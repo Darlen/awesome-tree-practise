967617eda537d7a7717c324222fe80af2fba71c2
tree <tree3170@gmail.com>
2018-04-06 12:54:59 +0800

    DataSourceConfig:配置数据源

---
+++ spring-annotations-skeleton/src/main/java/com/tree/config/DataSourceConfig.java
@@ -0,0 +1,57 @@
+package com.tree.config;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+/**
+ * @ClassName com.tree.config
+ * Description: <类功能描述>. <br>
+ * <p>
+ * <使用说明>
+ * </p>
+ * @Author tree
+ * @Date 2018/4/6 09:41
+ * @Version 1.0
+ */
+@Configuration
+//启用注解事务管理，使用CGLib代理
+@EnableTransactionManagement(proxyTargetClass = true)
+@PropertySource({"classpath:db.properties"})
+public class DataSourceConfig {
+    private static final Logger logger = LoggerFactory.getLogger(DataSourceConfig.class);
+
+    @Value("${jdbc.driver}")
+    private String driverClass;
+    @Value("${jdbc.url}")
+    private String jdbcUrl;
+    @Value("${jdbc.username}")
+    private String username;
+    @Value("${jdbc.password}")
+    private String password;
+
+
+    @Autowired
+    private Environment env;
+
+    @Bean
+    public DataSource dataSource(){
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName(driverClass);
+        dataSource.setUrl(jdbcUrl);
+        dataSource.setUsername(username);
+        dataSource.setPassword(password);
+        return dataSource;
+    }
+
+
+}
