b251d7fa15d4ddad387c6786cf4a0f2fbd5e4f9c
tree <tree3170@gmail.com>
2018-04-06 10:44:41 +0800

    配置AppConfig

--- spring-annotations-skeleton/src/main/java/com/tree/config/AppConfig.java
+++ spring-annotations-skeleton/src/main/java/com/tree/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.tree.config;
 
+import org.springframework.context.annotation.*;
+import org.springframework.stereotype.Controller;
+
 /**
  * com.tree.config
  * Description: <类功能描述>. <br>
@@ -11,6 +14,12 @@ package com.tree.config;
  * @Date 2018/4/5 18:41
  * @Version 1.0
  */
-
+@Configuration
+@ComponentScan(basePackages = "com.tree",
+        excludeFilters = {@ComponentScan.Filter(type = FilterType.ANNOTATION,value = {Controller.class})})
+@EnableAspectJAutoProxy(proxyTargetClass = true)
+@PropertySource(value =  {"classpath:db.properties","classpath:app.properties"})
+@Import({CacheConfig.class,DataSourceConfig.class})
 public class AppConfig {
+
 }
